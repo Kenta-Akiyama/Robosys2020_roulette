486a7049570dc114425b7b813011f427e2fd0d3f
Kenta Akiyama <49668445+Kenta-Akiyama@users.noreply.github.com>
2020-12-18 03:48:02 +0900

    インデントの修正

--- mydev.c
+++ mydev.c
@@ -28,52 +28,54 @@ static ssize_t led_write(struct file* flip, const char* buf, size_t count, loff_
 	int x = 0;
 
 	if(copy_from_user(&c, buf, sizeof(char)))
-	return -EFAULT;
-
+		return -EFAULT;
 
 	if(c == '0'){
 		for(j = 0; j < n; j++){
 
 			gpio_base[7] = 1 << pin[x];
+			
 			for(k = 0; k < 100 + 3*j; k++){
 				gpio_base[7] = 1 << 19;
 				udelay(1);
 				gpio_base[10] = 1 << 19;
 				udelay(1);
 			}
+			
 			mdelay(50 + 5*j);
 			gpio_base[10] = 1 << pin[x];
 			x++;
+			
 			if(x == 5){
 				x = 0;
+			}
 		}
-	
+		gpio_base[7] = 1 << pin[x];
+		
+		for(k = 0; k < 500000; k++){
+			gpio_base[7] = 1 << 19;
+			udelay(1);
+			gpio_base[10] = 1 << 19;
+			udelay(1);
+		}
 	}
-	gpio_base[7] = 1 << pin[x];
-	for(k = 0; k < 500000; k++){
-		gpio_base[7] = 1 << 19;
-		udelay(1);
-		gpio_base[10] = 1 << 19;
-		udelay(1);
-	}
-}
 
 	else if (c == '1'){
 		for(j = 0; j <=5; j++){
 			gpio_base[10] = 1 << pin[j];
-			}
+		}
 		if( n > 60){
-		n = n - pi[l];
-		l++;
+			n = n - pi[l];
+			l++;
 		}
 		else if(n <= 60){
-		n = n + pi[l];
-		l++;
+			n = n + pi[l];
+			l++;
 		}
 	}
 	if(l == 21){
 		l = 1;
-}
+	}
 	return 1;
 }
 
@@ -90,20 +92,23 @@ static int __init init_mod(void)
 	int retval;
 
 	retval = alloc_chrdev_region(&dev, 0, 1, "mydev");
+	
 	if(retval < 0){
-	printk(KERN_ERR "alloc_chrdev_region failed.\n");
-	return retval;
+		printk(KERN_ERR "alloc_chrdev_region failed.\n");
+		return retval;
 }
 
 	printk(KERN_INFO "%s is loaded, major:%d\n",__FILE__, MAJOR(dev));
 	
 	cdev_init(&cdv, &led_fops);
 	retval = cdev_add(&cdv, dev, 1);
+	
 	if(retval < 0){
 		printk(KERN_ERR "cdev_add failed. major:%d, minor:%d",MAJOR(dev),MINOR(dev));
 		return retval;
 	}
 	cls = class_create(THIS_MODULE, "mydev");
+	
 	if(IS_ERR(cls)){
 		printk(KERN_ERR "class_create failed.");
 		return PTR_ERR(cls);
@@ -114,11 +119,11 @@ static int __init init_mod(void)
 	gpio_base = ioremap_nocache(0xfe200000, 0xA0);
 
 	for(i = 0; i < N; i++){
-	const u32 index = pin[i]/10;
-	const u32 shift = (pin[i]%10)*3;
-	const u32 mask = ~(0x7 << shift);
-	gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
-}
+		const u32 index = pin[i]/10;
+		const u32 shift = (pin[i]%10)*3;
+		const u32 mask = ~(0x7 << shift);
+		gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
+	}
 
 	return 0;
 }
@@ -130,5 +135,6 @@ static void __exit cleanup_mod(void)
 	unregister_chrdev_region(dev, 1);
 	printk(KERN_INFO "%s is unloaded. major:%d\n",__FILE__, MAJOR(dev));
 }
+
 module_init(init_mod);
 module_exit(cleanup_mod);
